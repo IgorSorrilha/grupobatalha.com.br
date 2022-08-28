13e7b8972c3c7a0169f70dcc7930638f25eeb4a5
Igor Sorrilha <igor.sorrilha@gmail.com>
2022-08-27 21:40:25 -0300

    ajustes no header

--- application/views/html_header.php
+++ application/views/html_header.php
@@ -6,9 +6,9 @@
     <title><?php echo $title; ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="title" content="Reserva Nature Atibaia" />
-    <meta name="og:title" content="Reserva Nature Atibaia" />
-    <meta name="og:description" content="Loteamento fechado com terrenos a partir de 360M²" />
+    <meta name="title" content="Terroir³¹ Residencial Resort & Olivial" />
+    <meta name="og:title" content="Terroir³¹ Residencial Resort & Olivial" />
+    <meta name="og:description" content="Seja bem-vindo ao 1° Residencial Resort & Olival do Brasil." />
     <meta name="og:image" content="<?= base_url(); ?>assets/images/compartilhamento.png" />
     <link rel="icon" href="<?= base_url(); ?>assets/images/favicon.png">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= base_url(); ?>assets/images/apple-touch-icon-144-precomposed.png">
@@ -18,10 +18,10 @@
     <link rel="image_src" href="<?= base_url(); ?>assets/images/compartilhamento.png" />
     <meta name="theme-color" content="#dbe296" />
     <!-- property (faz funcionar no linkedin) -->
-    <meta property="og:title" content="Reserva Nature Atibaia" />
+    <meta property="og:title" content="Terroir³¹ Residencial Resort & Olivial" />
     <meta property="og:image" content="<?= base_url(); ?>assets/images/compartilhamento.png" />
-    <meta property="Loteamento fechado com terrenos a partir de 360M²" />
-    <meta property="og:url" content="https://www.reservaelementum.com.br" />
+    <meta property="Seja bem-vindo ao 1° Residencial Resort & Olival do Brasil." />
+    <meta property="og:url" content="https://www.grupobatalha.com.br" />
     <!-- property (faz funcionar no linkedin) -->
     <?php
     $meta = array(
