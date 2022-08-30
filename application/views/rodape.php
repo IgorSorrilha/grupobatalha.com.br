d92cf16cf7221096e592962a05e84c928075a6e3
Igor Sorrilha <igor.sorrilha@gmail.com>
2022-08-30 00:40:30 -0300

    alt img rodape

--- application/views/rodape.php
+++ application/views/rodape.php
@@ -2,7 +2,7 @@
     <div class="container-fluid padding-off realizacao">
         <div class="container">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3 padding-off">
-                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/realizacao-footer.png">
+                <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/realizacao-footer.png" alt="Terroir³¹ Residencial Resort & Olivial">
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
                 <p>IMAGENS MERAMENTE ILUSTRATIVAS, SUJEITO À ALTERAÇÕES. <br class="visible-xs">O EMPREENDIMENTO <br class="hidden-xs">
